a57e051fb63216a3473e120d76335b652f0af318
Timur <timurka.askarov@inbox.ru>
2024-07-20 16:52:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vkcryptoapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.vkcryptoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.vkcryptoapp.presentation.screens.HomeScreen
+import com.example.vkcryptoapp.ui.theme.VkCryptoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VkCryptoAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/vkcryptoapp/presentation/screens/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.vkcryptoapp.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
